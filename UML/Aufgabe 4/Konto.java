9c3290853e889f1ff0d49cb57ae3f7d6f181e144
16uhrpasing <juric.luka@protonmail.com>
2018-10-25 13:21:46 +0200

    Konstruktoren bearbeitet

--- UML/Aufgabe 4/Konto.java
+++ UML/Aufgabe 4/Konto.java
@@ -4,7 +4,13 @@ public class Konto{
 	private Kunde[] zeichnungsberechtigter;
 	
 	public GeldBetrag saldo(){
-		
+		return GeldBetrag;
+	}
+
+	public Konto(Kunde Hauptzeichnungsberechtigter)
+	{
+		zeichnungsberechtigter[0] = Hauptzeichnungsberechtigter; //Hauptbesitzer vom Konto da gelten muss 1..*
+
 	}
 	
 	public void einzahlen(GeldBetrag betrag){
--- UML/Aufgabe 4/Kunde.java
+++ UML/Aufgabe 4/Kunde.java
@@ -1,7 +1,11 @@
 public abstract class Kunde{
 	
 	private Konto[] konto;
-	
+
+	public Kunde(Konto Hauptkonto)
+	{
+		konto[0] = Hauptkonto; //Hauptkonto da gelten muss 1..*
+	}
 	
 
 }
