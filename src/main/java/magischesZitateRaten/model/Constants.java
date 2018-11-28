c72bd37e68c78d94e0ac6f35f5c5248a5a9767b7
MarcoEberle <marcoeberle@gmx.de>
2018-11-28 17:31:50 +0100

    fix

--- src/main/java/magischesZitateRaten/model/Constants.java
+++ src/main/java/magischesZitateRaten/model/Constants.java
@@ -7,16 +7,16 @@ public class Constants {
 
     public static boolean USE_CARDS_FLAG = true;
 
-    public static String WELCOME_MESSAGE = "Danke für das Öffnen von Magische Zitate Raten. In diesem Spiel geht es darum," +
+    public static String WELCOME_MESSAGE = "Danke fÃ¼r das Ã–ffnen von Magische Zitate Raten. In diesem Spiel geht es darum," +
             " alleine oder gegen einen zweiten Spieler Zitate zu erraten. Der Gewinner steht an der Spitze des High-Scores. Spielst du alleine oder zu zweit?";
 
     // This is the message a user will hear when they try to cancel or stop the
     // skill, or when they finish a quiz.
-    public static String EXIT_SKILL_MESSAGE = "Vielen Dank für das Spielen von Magisches Zitate Raten. Bis bald!";
+    public static String EXIT_SKILL_MESSAGE = "Vielen Dank fÃ¼r das Spielen von Magisches Zitate Raten. Bis bald!";
 
     // This is the message a user will hear when they ask Alexa for help in your
     // skill.
-    public static String HELP_MESSAGE = "Danke für das Öffnen von Magische Zitate Raten. In diesem Spiel geht es darum, alleine oder gegen einen zweiten Spieler Zitate zu erraten. Der Gewinner steht an der Spitze des High-Scores.";
+    public static String HELP_MESSAGE = "Danke fÃ¼r das Ã–ffnen von Magische Zitate Raten. In diesem Spiel geht es darum, alleine oder gegen einen zweiten Spieler Zitate zu erraten. Der Gewinner steht an der Spitze des High-Scores.";
 
     public static List<String> CORRECT_RESPONSES = Arrays.asList("Booya", "All righty", "Bam", "Bazinga", "Bingo", "Boom", "Bravo", "Cha Ching", "Cheers", "Dynomite",
             "Hip hip hooray", "Hurrah", "Hurray", "Huzzah", "Oh dear.  Just kidding.  Hurray", "Kaboom", "Kaching", "Oh snap", "Phew",
