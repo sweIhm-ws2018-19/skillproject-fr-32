b356100fc8e62c04a8f6afc5cca115eb254ee2c0
16uhrpasing <juric.luka@protonmail.com>
2018-12-14 12:00:59 +0100

    new tests

---
+++ src/test/java/TestGamePlayIntentTwoPlayers.java
@@ -0,0 +1,33 @@
+package com.amazon.ask.test.java;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import com.amazon.ask.dispatcher.request.handler.HandlerInput;
+import org.junit.Before;
+import org.mockito.Mockito;
+import com.amazon.ask.quiz.handlers.GamePlayIntentTwoPlayers;
+
+import static com.amazon.ask.request.Predicates.intentName;
+import static com.amazon.ask.request.Predicates.sessionAttribute;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+public class TestGamePlayIntentTwoPlayers {
+
+    private GamePlayIntentTwoPlayers handler;
+
+    @Before
+    public void setup() {
+        handler = new GamePlayIntentTwoPlayers();
+    }
+
+    @Test
+    public void testCanHandle() {
+        final HandlerInput inputMock = Mockito.mock(HandlerInput.class);
+        when(inputMock.matches(any())).thenReturn(true);
+        assertTrue(handler.canHandle(inputMock));
+    }
+
+}
