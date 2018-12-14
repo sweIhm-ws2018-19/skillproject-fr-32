35ea8fe4ba7b91c01710a766f2066db696731469
16uhrpasing <juric.luka@protonmail.com>
2018-12-14 11:38:48 +0100

    tests

---
+++ src/test/java/TestSetOnePlayerIntent.java
@@ -0,0 +1,33 @@
+package com.amazon.ask.test.java;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import com.amazon.ask.dispatcher.request.handler.HandlerInput;
+import org.junit.Before;
+import org.mockito.Mockito;
+import com.amazon.ask.quiz.handlers.SetOnePlayerIntent;
+
+import static com.amazon.ask.request.Predicates.intentName;
+import static com.amazon.ask.request.Predicates.sessionAttribute;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+public class TestSetOnePlayerIntent {
+
+    private SetOnePlayerIntent handler;
+
+    @Before
+    public void setup() {
+        handler = new SetOnePlayerIntent();
+    }
+
+    @Test
+    public void testCanHandle() {
+        final HandlerInput inputMock = Mockito.mock(HandlerInput.class);
+        when(inputMock.matches(any())).thenReturn(true);
+        assertTrue(handler.canHandle(inputMock));
+    }
+
+}
