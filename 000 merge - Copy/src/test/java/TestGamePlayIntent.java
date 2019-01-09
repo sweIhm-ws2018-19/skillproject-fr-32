e084376ddd373568cb48d972eb5997c3a5fe97ac
16uhrpasing <juric.luka@protonmail.com>
2019-01-09 18:24:42 +0100

    tests

--- 000 merge - Copy/src/test/java/TestGamePlayIntent.java
+++ 000 merge - Copy/src/test/java/TestGamePlayIntent.java
@@ -8,13 +8,19 @@ import org.mockito.Mockito;
 import com.amazon.ask.quiz.handlers.GamePlayIntent;
 import com.amazon.ask.model.Response;
 import java.util.Optional;
+import com.amazon.ask.quiz.util.QuestionDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static com.amazon.ask.request.Predicates.intentName;
 import static com.amazon.ask.request.Predicates.sessionAttribute;
 
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TestGamePlayIntent {
 
@@ -26,21 +32,47 @@ public class TestGamePlayIntent {
     }
 
     @Test
+    public void testCtor() {
+        assertEquals(handler.getClass(), GamePlayIntent.class);
+    }
+
+    @Test
     public void testCanHandle() {
         final HandlerInput inputMock = Mockito.mock(HandlerInput.class);
         when(inputMock.matches(any())).thenReturn(true);
         assertTrue(handler.canHandle(inputMock));
     }
 
-    @Test(expected = NullPointerException.class)
-    public void testHandle() {
-        final HandlerInput mockInput = TestUtil.mockHandlerInput(null, null, null, null);
-        final Optional<Response> res = handler.handle(mockInput);
+    @Test
+    public void nullHandleTest(){
+        assertThrows(NullPointerException.class, () -> handler.handle(null));
+    }
+
+    @Test
+    public void handleSlotNotNullTest(){
+        final Map<String, Object> sessionAttributes = new HashMap<>();
+        final Map<String, Object> persistentAttributes = new HashMap<>();
+        final Map<String, String> slots = new HashMap<>();
+        slots.put("Movies", "Mulan");
+        slots.put("quizscore", "0");
+        slots.put("counter", "0");
+
+        QuestionDatabase.initialize();
+
+        sessionAttributes.put("quizscore", 0);
+        sessionAttributes.put("counter", 0);
+
+        final HandlerInput inputMock = TestUtil.mockHandlerInput(slots, sessionAttributes, persistentAttributes, null);
+        final Optional<Response> res = handler.handle(inputMock);
+
 
         assertTrue(res.isPresent());
         final Response response = res.get();
-        assertTrue(response.getOutputSpeech().toString().length() >= 0);
-        assertTrue(response.getReprompt().toString().length() >= 0);
+
+        assertFalse(response.getShouldEndSession());
+        assertNotNull(response.getOutputSpeech());
+        assertTrue(response.getOutputSpeech().toString().contains("Das"));
     }
+
 
 }
--- src/test/java/TestGamePlayIntentTwoPlayers.java
+++ src/test/java/TestGamePlayIntentTwoPlayers.java
@@ -6,16 +6,22 @@ import com.amazon.ask.dispatcher.request.handler.HandlerInput;
 import org.junit.Before;
 import org.mockito.Mockito;
 import com.amazon.ask.quiz.handlers.GamePlayIntentTwoPlayers;
+import com.amazon.ask.quiz.model.Attributes;
 import com.amazon.ask.model.Response;
 import java.util.Optional;
+import com.amazon.ask.quiz.util.QuestionDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static com.amazon.ask.request.Predicates.intentName;
 import static com.amazon.ask.request.Predicates.sessionAttribute;
 
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TestGamePlayIntentTwoPlayers {
 
@@ -27,21 +33,49 @@ public class TestGamePlayIntentTwoPlayers {
     }
 
     @Test
+    public void testCtor() {
+        assertEquals(handler.getClass(), GamePlayIntentTwoPlayers.class);
+    }
+
+    @Test
     public void testCanHandle() {
         final HandlerInput inputMock = Mockito.mock(HandlerInput.class);
         when(inputMock.matches(any())).thenReturn(true);
         assertTrue(handler.canHandle(inputMock));
     }
 
-    @Test(expected = NullPointerException.class)
-    public void testHandle() {
-        final HandlerInput mockInput = TestUtil.mockHandlerInput(null, null, null, null);
-        final Optional<Response> res = handler.handle(mockInput);
+    @Test
+    public void nullHandleTest(){
+        assertThrows(NullPointerException.class, () -> handler.handle(null));
+    }
+
+    @Test
+    public void handleSlotNotNullTest(){
+        final Map<String, Object> sessionAttributes = new HashMap<>();
+        final Map<String, Object> persistentAttributes = new HashMap<>();
+        final Map<String, String> slots = new HashMap<>();
+        slots.put("Movies", "Mulan");
+
+        QuestionDatabase.initialize();
+
+        sessionAttributes.put(Attributes.PLAYER_NUMBER_KEY, 0);
+        sessionAttributes.put(Attributes.SCORE_PLAYER_ONE, 0);
+        sessionAttributes.put(Attributes.SCORE_PLAYER_TWO, 0);
+        sessionAttributes.put(Attributes.CURRENT_QUESTION_OF_PLAYER_ONE, 0);
+        sessionAttributes.put(Attributes.CURRENT_QUESTION_OF_PLAYER_TWO, 0);
+        sessionAttributes.put(Attributes.COUNTER_KEY, 0);
+
+        final HandlerInput inputMock = TestUtil.mockHandlerInput(slots, sessionAttributes, persistentAttributes, null);
+        final Optional<Response> res = handler.handle(inputMock);
+
 
         assertTrue(res.isPresent());
         final Response response = res.get();
-        assertTrue(response.getOutputSpeech().toString().length() >= 0);
-        assertTrue(response.getReprompt().toString().length() >= 0);
+
+        assertFalse(response.getShouldEndSession());
+        assertNotNull(response.getOutputSpeech());
+        assertTrue(response.getOutputSpeech().toString().contains("Das"));
     }
+
 
 }
